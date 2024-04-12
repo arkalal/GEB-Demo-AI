e0eef2bf028ed2a8e22cc8d00b7532fe2e510c5d
arkalal <arkalal.123@gmail.com>
2024-04-12 18:01:41 +0530

    move to prod

--- axios/baseUrl.js
+++ axios/baseUrl.js
@@ -1,2 +1,2 @@
 export const baseUrlTest = "http://localhost:3000";
-export const baseUrlStaging = "https://gen-ai-playground.vercel.app";
+export const baseUrlStaging = "https://geb-demo.vercel.app";
